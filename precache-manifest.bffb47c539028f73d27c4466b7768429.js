18c7147030bf814a63745779bcde37911a8eab7d
Jasan Hernandez <jasan_azael@hotmail.es>
2019-06-01 17:18:17 -0500

    Updates

---
+++ precache-manifest.bffb47c539028f73d27c4466b7768429.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "5bb823f257e4df98906a",
+    "url": "/Invie-animated/static/css/main.7d80a0d5.chunk.css"
+  },
+  {
+    "revision": "5bb823f257e4df98906a",
+    "url": "/Invie-animated/static/js/main.5bb823f2.chunk.js"
+  },
+  {
+    "revision": "33332c39c9a13cce3426",
+    "url": "/Invie-animated/static/js/1.33332c39.chunk.js"
+  },
+  {
+    "revision": "581731a66db24899f354",
+    "url": "/Invie-animated/static/js/runtime~main.581731a6.js"
+  },
+  {
+    "revision": "25c6f861860963cb105716cc999b5174",
+    "url": "/Invie-animated/static/media/invie-acustica.25c6f861.png"
+  },
+  {
+    "revision": "e039fff97126b1b18d68f4be45bf6d25",
+    "url": "/Invie-animated/static/media/invie-classic.e039fff9.png"
+  },
+  {
+    "revision": "ba2e415e817351fb402d07afd2be5ceb",
+    "url": "/Invie-animated/static/media/easter-a.ba2e415e.png"
+  },
+  {
+    "revision": "7d7a81dcaa54ebefe86d47f90c6bb589",
+    "url": "/Invie-animated/static/media/easter-b.7d7a81dc.png"
+  },
+  {
+    "revision": "e9b52209dbaac935c802f5ed9b4102ef",
+    "url": "/Invie-animated/static/media/background.e9b52209.png"
+  },
+  {
+    "revision": "70297162b2ac4eb6ac54a0ddd5f85bfe",
+    "url": "/Invie-animated/static/media/team-platzi-azul.70297162.jpg"
+  },
+  {
+    "revision": "6d0f38aaa66022eef8477e62b86c8283",
+    "url": "/Invie-animated/static/media/background2x.6d0f38aa.png"
+  },
+  {
+    "revision": "313656d46d6613b2d25f50a785361e1c",
+    "url": "/Invie-animated/static/media/heart.313656d4.png"
+  },
+  {
+    "revision": "dfe12908e70aac387c6a17d478900752",
+    "url": "/Invie-animated/index.html"
+  }
+];
